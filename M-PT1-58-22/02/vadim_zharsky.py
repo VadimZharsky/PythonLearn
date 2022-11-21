60cdf551cf92d31f155b5168e9f681ff999a7390
VadimZharsky <vadimzharsky1990@gmail.com>
2022-11-21 16:31:54 +0300

    lesson02_ended

--- M-PT1-58-22/02/vadim_zharsky.py
+++ M-PT1-58-22/02/vadim_zharsky.py
@@ -1,25 +1,41 @@
 
-
-def triangle_is_valid(a, b, c):
-    if (a+b)<=c:
-        return f"Triangle is invalid. The sum of first side {a} and second side {b} is {a+b} and must be greater than third side {c}" 
-    if (a+c)<=b:
-        return f"Triangle is invalid. The sum of first side {a} and third side {c} is {a+c} and must be greater than second side {b}"
-    if (b+c)<=a:
-        return f"Triangle is invalid. The sum of second side {b} and third side {c} is {b+c} and must be greater than first side {a}"
-    return "triangle is valid"
-
 def main():
     print("Triange Verificator\nFor verify triangle you need to input 3 side lengths")
+    sides_array = []
     try:
-        first_side = float(input("input first side length: "))
-        second_side = float(input("input second side length: "))
-        third_side = float(input("input third side length: "))
-        result = triangle_is_valid(first_side, second_side, third_side)
+        sides_array.append(float(input("input first side length: ")))
+        sides_array.append(float(input("input second side length: ")))
+        sides_array.append(float(input("input third side length: ")))
+        result = triangle_is_valid(sides_array)
         print(result)
     except ValueError:
         print("Invalid input.")
 
+def triangle_is_valid(arr):
+    equilateral = is_equilateral(arr)
+    if (equilateral is True):
+        return("Triangle is valid. Equilateral triangle is builded")
+    arr.sort()
+    #To check validity of triangle we need sum of two sides greater than the base 
+    if((arr[0]+arr[1])<=arr[2]):
+        return f"Triangle is invalid. The sum of first side {arr[0]} and second side {arr[1]} is {arr[0]+arr[1]} and must be greater than the base {arr[2]}"
+    isosceles = is_isosceles(arr[0], arr[1])
+    if (isosceles is True):
+        return(f"Triangle is valid. Isosceles triangle is builded with two equal sides {arr[0]} and base {arr[2]}")
+    else:
+        return(f"Triangle is valid. Scalene triangle is builded with {arr[0]} side, {arr[1]} second side and a base {arr[2]}")
+
+def is_equilateral(arr):
+    #all sides are equal
+    if (arr[0] == arr[1] and arr[0] == arr[2]):
+        return True
+    return False
+
+def is_isosceles(first, second):
+    #two sides are equal
+    if (first == second):
+        return True
+    return False
     
 
 if __name__ == '__main__':
