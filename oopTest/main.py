f9ce8f99c1dec700809035debce015a538aa12ac
VadimZharsky <vadimzharsky1990@gmail.com>
2022-11-22 16:10:02 +0300

    angle_sum

--- oopTest/main.py
+++ oopTest/main.py
@@ -1,6 +1,11 @@
+from lib.triagle import Triangle
 
 def main():
-    pass
+    triange1 = Triangle(6, 6, 8.4852)
+    print(triange1.get_sides())
+    triange1.is_valid()
+    triange1.get_angles()
+
 
 if __name__ == '__main__':
     main()
