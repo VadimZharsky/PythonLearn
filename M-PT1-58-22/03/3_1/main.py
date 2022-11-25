fa4e9c6b3d2bd959d14bc40f822778e5dd9e3ba8
VadimZharsky <vadimzharsky1990@gmail.com>
2022-11-25 12:59:21 +0300

    lesson 3_1

--- M-PT1-58-22/03/3_1/main.py
+++ M-PT1-58-22/03/3_1/main.py
@@ -1,11 +1,39 @@
+from itertools import starmap, repeat, compress
+from operator import eq
+import time
 
 def main():
     line = "one two three eleven one two three eleven one two three eleven"
-    result = max_finder(line)
-    print(result)
+    words = line.split(" ")
 
-def max_finder(line):
-    words= line.split(" ")
+    start = time.time()
+    result = max_finder(words)
+    end = time.time()
+    speed = end - start
+    print(f"cycle max:\n{result}\nended with {speed}\n\n")
+    
+    start = time.time()
+    result = for_finder(words)
+    end = time.time()
+    speed = end - start
+    print(f"cycle for_finder:\n{result}\nended with {speed}\n\n")
+
+    start = time.time()
+    result = without_for(words)
+    end = time.time()
+    speed = end - start
+    print(f"cycle without_for:\n{result}\nended with {speed}\n\n")
+
+
+
+def without_for(words):
+    lengths = list(map(len, words))
+    max_length = max(lengths)
+    max_indices = starmap(eq, zip(lengths, repeat(max_length)))
+    max_words = list(compress(words, max_indices))
+    return max_words
+
+def max_finder(words):
     curmax = 0
     result = []
     for w in words:
@@ -22,6 +50,16 @@ def max_finder(line):
             result = [w]
     
     return result
+
+def for_finder(words):
+    max_length = max(map(len, words))
+    result = [
+        w
+        for w in words
+        if len(w) == max_length
+        ]
+    return result
+
 
 if __name__ =="__main__":
     main()
