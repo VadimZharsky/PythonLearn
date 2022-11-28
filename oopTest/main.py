de7b56ac4d610ec897402a99927ac66198e0152b
VadimZharsky <vadimzharsky1990@gmail.com>
2022-11-28 11:36:33 +0300

    trying branch

--- oopTest/main.py
+++ oopTest/main.py
@@ -1,10 +1,13 @@
 from lib.triagle import Triangle
 
+
 def main():
-    instructor("tringle1", [4,5,6])
-    instructor("tringle2", [8,9,8])
-    instructor("tringle3", [34,15,20])
-    instructor("tringle4", [15,15,15])
+    instructor("tringle1", [4, 5, 6])
+    instructor("tringle2", [8, 9, 8])
+    instructor("tringle3", [34, 15, 20])
+    instructor("tringle4", [15, 15, 15])
+    instructor("tringle5", [24, 28, 30])
+
 
 def instructor(name, sides):
     name = Triangle(sides[0], sides[1], sides[2])
