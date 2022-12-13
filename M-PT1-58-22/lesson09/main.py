90f17174b00c9bbd5526375a302c69357399b704
VadimZharsky <vadimzharsky1990@gmail.com>
2022-12-13 17:49:07 +0300

    somthng happened

--- M-PT1-58-22/lesson09/main.py
+++ M-PT1-58-22/lesson09/main.py
@@ -1,15 +1,37 @@
+import time
+import pytest
+
 import tasks
 
 def main():
-    # print(tasks.task_01_do_twice("py"))
-
     # assert tasks.task_01_do_twice("text") == "texttext"
     # assert tasks.task_01_do_twice("python") == "pythonpython"
 
     # result = tasks.task_02_count_calls()
-    result = [tasks.task_02_count_calls() for _ in [1, 2, 3, 4]]
+    counter: int = 0
+    assert not counter
+    result = [tasks.test_02() for _ in range(0, 3)][-1]
     print(result)
 
+    # res1 = tasks.task_03_cashe("text_to cashe")
+    # res3 = tasks.task_03_cashe("another text")
+    # z = [tasks.task_03_cashe("text_to cashe") for _ in range(0, 3)][-1]
+    # assert z is res1
+    # assert not z is res3
+
+    # time_sleep = 2
+    # start_time = time.monotonic()
+    # tasks.task_04_benchmark(time_sleep)
+    # end_time = time.monotonic() - start_time
+    # assert abs(end_time - time_sleep)<0.1
+    
+    # assert tasks.f(4, 8) == 32
+    # assert tasks.f(133, 56) == 7448
+    # with pytest.raises(TypeError):
+    #     assert tasks.f(2, 2.3)
+    # with pytest.raises(TypeError):
+    #     assert tasks.g()
+        
 
 if __name__ == "__main__":
     main()
--- M-PT1-58-22/lesson09/tasks.py
+++ M-PT1-58-22/lesson09/tasks.py
@@ -1,3 +1,31 @@
+from typing import Any
+from time import sleep
+import pytest
+
+
+def typecheck(func):
+    def checker(*args):
+        if len(args) != 0:
+            if ([isinstance(x, int) for x in args][-1]):
+                return func(args[0], args[1])
+            else:
+                raise TypeError(f"{args=!r} is not type of int")
+        else:
+            raise TypeError(f"{args=!r} is not type of int")
+    return checker
+
+
+def cashe_creator(func):
+    memory = {}
+    def inner(*args):
+        if args in memory:
+            return memory[args]
+        else:
+            rec_new = func(*args)
+            memory[args] = rec_new
+            return rec_new
+    return inner
+
 
 class ExecutionCounter:
     call_count:int = 0 
@@ -6,13 +34,6 @@ class ExecutionCounter:
     def __call__(self, *args, **kwargs):
         self.call_count += 1
         return self.call_count
-
-def execute_counter(func) -> int:
-    call_count:int = 0 
-    def __call__():
-        call_count += 1
-        return call_count
-    return __call__
 
 
 def do_twice(func) -> str:
@@ -27,6 +48,23 @@ def do_twice(func) -> str:
 def task_01_do_twice(text: str) -> str:
     return(text)
 
-@execute_counter
-def task_02_count_calls() -> int:
+@ExecutionCounter
+def test_02() -> int:
     return 0
+
+@cashe_creator
+def task_03_cashe(anything: Any) -> Any:
+    return anything
+
+@cashe_creator
+def task_04_cashe(sleep_time: int) -> Any:
+    sleep(sleep_time)
+    return sleep_time
+
+@typecheck
+def g() -> int:
+    return "1"
+
+@typecheck
+def f(num: int, mul: int) -> int:
+    return num * mul
